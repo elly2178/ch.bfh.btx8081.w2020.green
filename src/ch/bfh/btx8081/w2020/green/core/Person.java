80f5546a753f66113ad4151e21eb7abe7ee804ff
norman <jimen1@bfh.ch>
2020-11-21 17:17:41 +0100

    Class Diagram Deployment Diagram Component Diagram

---
+++ src/ch/bfh/btx8081/w2020/green/core/Person.java
@@ -0,0 +1,4 @@
+package ch.bfh.btx8081.w2020.green.core;
+
+public abstract class Person {
+}
--- src/ch/bfh/btx8081/w2020/green/core/TestClass.java
+++ src/ch/bfh/btx8081/w2020/green/core/TestClass.java
@@ -1,7 +1,15 @@
 package ch.bfh.btx8081.w2020.green.core;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public class TestClass extends VerticalLayout {
-	
+
+    public TestClass() {
+        Button button = new Button("Click me",
+                event -> Notification.show("Clicked!"));
+        add(button);
+    }
+
 }
