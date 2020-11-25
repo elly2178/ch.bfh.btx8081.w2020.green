699b05e2bef469258cdcaf5bc5400eb67c14abfe
Daniel Schmid <daniel.schmid@wuestpartner.com>
2020-11-25 11:38:16 +0100

    commented code realized

--- src/main/java/core/MainView.java
+++ src/main/java/core/MainView.java
@@ -2,7 +2,9 @@ package core;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
@@ -13,14 +15,13 @@ import com.vaadin.flow.server.PWA;
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
-    	VerticalLayout vertical = new VerticalLayout ();
-    	//HorizontalLayout horizontal = new HorizontalLayout ();
-    	//vertical.addComponent(horizontal);
-    	//horizontal.addComponent(new Textfield("Patient Search"));
-    	//horizontal.addComponent(new Textfield("+ Add New Patient"));
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(vertical, button);
-    }
+	public MainView() {
+		VerticalLayout vertical = new VerticalLayout();
+		HorizontalLayout horizontal = new HorizontalLayout();
+		vertical.add(horizontal);
+		horizontal.add(new TextField("Patient Search"));
+		horizontal.add(new TextField("+ Add New Patient"));
+		Button button = new Button("Click me", event -> Notification.show("Clicked!"));
+		add(vertical, button);
+	}
 }
