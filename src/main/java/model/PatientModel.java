7a04fc8539716fd857b6196086634e435fd167b3
elly2178 <dumitru.georgiana_geanina@yahoo.com>
2020-12-02 19:27:11 +0100

    Removed useless and confusing Patient class. dont restore pls

--- src/main/java/model/PatientModel.java
+++ src/main/java/model/PatientModel.java
@@ -1,18 +1,28 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import common.Address; 
+import common.Person;
 
-import common.Address;
-import common.Patient;
-
-public class PatientModel {
+public class PatientModel extends Person{
+	
 	int ybocs;
 	int mood;
 	int combined = ybocs + mood;
+	private int pid;
+	private String healthInsuranceName;
+	private ArrayList<String> diaries = new ArrayList();
 	
+	public PatientModel(String firstName, String secondName, LocalDate birthDate, boolean gender, Address address,
+			String email) {
+		super(firstName, secondName, birthDate, gender, address, email);
+		// TODO Auto-generated constructor stub
+	}
+
 	LocalDate birthDate = LocalDate.of(1989, 05, 18);
 	Address patAddress = new Address(2502, "Freistrasse", "Biel", "Schweiz");
-	Patient pat1 = new Patient("Georgiana", "Dumitru", birthDate, false, patAddress, "random@email.com");
+	//Patient pat1 = new Patient("Georgiana", "Dumitru", birthDate, false, patAddress, "random@email.com");
 	
 	//calculateAVr score
 	public float calculateAvarageScore() {
@@ -20,5 +30,19 @@ public class PatientModel {
 		float avarageScore = (float) combined / inputTime;
 		return avarageScore;
 		
+	}
+	
+	public boolean isAdult() {
+		LocalDate dateToday = java.time.LocalDate.now();
+		LocalDate birthDate = LocalDate.of(1989, 05, 18);
+		 
+		int yearDiff = dateToday.getYear() - birthDate.getYear();
+				
+		if (yearDiff >= 18) {
+			return true;
+		} else {
+			return false;
+		}
+		 
 	}
 }
