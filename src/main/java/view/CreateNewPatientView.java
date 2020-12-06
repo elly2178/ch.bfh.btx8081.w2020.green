8d09e01d2ca2f3a1179d48518be1f5e0fce93b7e
tange1 <edisonroberto.tangarifebonilla@students.bfh.ch>
2020-12-06 13:34:52 +0100

    .

--- src/main/java/view/CreateNewPatientView.java
+++ src/main/java/view/CreateNewPatientView.java
@@ -1,8 +1,10 @@
 package view;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("newPatient")
@@ -14,7 +16,31 @@ public class CreateNewPatientView extends VerticalLayout {
 	
 	Button button = new Button("Click me ", event -> Notification.show("Clicked!"));
 	
-	add(button);
+	FormLayout columnLayout = new FormLayout();
+	// Setting the desired responsive steps for the columns in the layout
+	//columnLayout.setResponsiveSteps();
+	           //new ResponsiveStep("25em", 1),
+	           //new ResponsiveStep("32em", 2),
+	          // new ResponsiveStep("40em", 3));
+	   TextField firstName = new TextField();
+	   firstName.setPlaceholder("First Name");
+	   TextField lastName = new TextField();
+	   lastName.setPlaceholder("Last Name");
+	   TextField email = new TextField();
+	   email.setPlaceholder("Email");
+	   TextField birthdate = new TextField();
+	   birthdate.setPlaceholder("Birthdate");
+	   TextField website = new TextField();
+	   website.setPlaceholder("Link to personal website");
+	   TextField description = new TextField();
+	   description.setPlaceholder("Enter a short description about yourself");
+	   columnLayout.add(firstName, lastName,  birthdate, email, website); 
+	   // You can set the desired column span for the components individually.
+	   columnLayout.setColspan(website, 2);
+	   // Or just set it as you add them.
+	   columnLayout.add(description, 3);
+	   Button buttonSaved = new Button("Save new patient", event -> Notification.show("Saved!"));
+	   add(button, columnLayout, buttonSaved);
 	
 	}
 
