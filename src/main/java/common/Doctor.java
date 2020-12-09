fc04ad2f3dc6c0d3c31c7523b82f303d53ea08dc
norman <jimen1@bfh.ch>
2020-12-09 03:37:21 +0100

    Added basic code Doctor

--- src/main/java/common/Doctor.java
+++ src/main/java/common/Doctor.java
@@ -1,19 +1,48 @@
 package common;
 
+import model.Patient;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class representing a doctor.
  */
+
 public class Doctor extends Person {
 
+    private int id;
     private String title;
     private String location;
+    private List<Patient> patients = new ArrayList<>();
 
 
     public Doctor(String firstName, String secondName, LocalDate birthDate, boolean gender, Address address, String email, String phone) {
         super(firstName, secondName, birthDate, gender, address, email, phone);
     }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(List<Patient> patients) {
+        this.patients = patients;
+    }
 }
