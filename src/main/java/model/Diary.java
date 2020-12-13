55d3d30215c764095143a7740988453c688761aa
Norman A. Jiménez <jimen1@bfh.ch>
2020-12-13 16:23:13 +0100

    Added JPA Querries, expanded DBManager, Diaries class also. JPA Annotations also added.

--- src/main/java/model/Diary.java
+++ src/main/java/model/Diary.java
@@ -1,12 +1,54 @@
 package model;
 
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "diaries")
 public class Diary {
 
+    @Id
+    private String id;
+
+    @ManyToOne
+    public List<String> diariestext = new ArrayList<>();
+
+    @Column(name = "entries")
     private String dailyEntry;
 
-    public Diary(){
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
 
+    public Diary(){
     }
 
+    public Diary(String dailyEntry, Date creationDate){
+        this.dailyEntry = dailyEntry;
+        this.creationDate = new Date(creationDate.getTime());
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Id
+    public String getId() {
+        return id;
+    }
+
+    public String getDailyEntry() {
+        return dailyEntry;
+    }
+
+    public void setDailyEntry(String dailyEntry) {
+        this.dailyEntry = dailyEntry;
+    }
+
+    public void addEntry(){
+        diariestext.add(this.dailyEntry);
+    }
 }
--- src/main/java/test/testDB.java
+++ src/main/java/test/testDB.java
@@ -1,66 +1,68 @@
-//package test;
-//
-//import model.Doctor;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.EntityTransaction;
-//import javax.persistence.Persistence;
-//import javax.persistence.Query;
-//
-//public class testDB {
-//
-//    private static final String PERSISTENCE_UNIT_NAME = "doctor";
-//    private EntityManager em;
-//    private Object Address;
-//    private Object Doctor;
-//
-//    public testDB() {
-//        em = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME).createEntityManager();
-//    }
-//
-//    public void setUp() {
-//        EntityTransaction transaction = em.getTransaction();
-//        transaction.begin();
-//
-//        Query q = em.createQuery("select d from Doctor d");
-//
-//        boolean createNewEntries = (q.getResultList().size() == 0);
-//
-//        if(createNewEntries){
-//            Doctor d = new Doctor();
-//            em.persist(Doctor);
-//            d.setTitle("Prof. dr.");
-//            d.setFirstName("Norman");
-//            d.setSecondName("Benkert");
-//            d.setGender(false);
-//            d.setAddress(null);
-//            d.setEmail("nobe@leafdoctors.io");
-//            d.setPhone("+417978789");
-//
-//            em.persist(d);
-//        }
-//
-//        em.flush();
-//        transaction.commit();
-//    }
-//
-//    public void checkAvailablePeople(){}
-//    Query q = em.createQuery("select d from Doctor d");
-//
-//    public void close(){
-//        EntityTransaction transaction = em.getTransaction();
-//        transaction.begin();
-//        Query q = em.createNativeQuery("SHUTDOWN COMPACT");
-//        q.executeUpdate();
-//        em.flush();
-//        transaction.commit();
-//        em.close();
-//    }
-//
-//    public static void main(String[] args) {
-//        testDB test = new testDB();
-//        test.setUp();
-//        test.checkAvailablePeople();
-//        test.close();
-//    }
-//}
+/*
+package test;
+
+import model.Doctor;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class testDB {
+
+   private static final String PERSISTENCE_UNIT_NAME = "ch.bfh.btx8081.w2020";
+    private EntityManager em;
+    private Object Address;
+    private Object Doctor;
+
+    public testDB() {
+        em = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME).createEntityManager();
+   }
+
+   public void setUp() {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Query q = em.createQuery("select d from Doctor d");
+
+        boolean createNewEntries = (q.getResultList().size() == 0);
+
+        if(createNewEntries){
+            Doctor d = new Doctor();
+            em.persist(Doctor);
+            d.setTitle("Prof. dr.");
+            d.setFirstName("Norman");
+            d.setSecondName("Benkert");
+            d.setGender(false);
+            d.setAddress(null);
+            d.setEmail("nobe@leafdoctors.io");
+            d.setPhone("+417978789");
+
+            em.persist(d);
+        }
+
+        em.flush();
+        transaction.commit();
+    }
+
+    public void checkAvailablePeople(){}
+    Query q = em.createQuery("select d from Doctor d");
+
+   public void close(){
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        Query q = em.createNativeQuery("SHUTDOWN COMPACT");
+        q.executeUpdate();
+        em.flush();
+        transaction.commit();
+        em.close();
+    }
+
+    public static void main(String[] args) {
+        testDB test = new testDB();
+        test.setUp();
+        test.checkAvailablePeople();
+        test.close();
+    }
+}
+*/
