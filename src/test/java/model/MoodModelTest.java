43b08857a9c3e8c22359c63c59fc5473944ca335
elly2178 <dumitru.georgiana_geanina@yahoo.com>
2020-12-16 18:36:51 +0100

    first junitTests for Class Mood

--- src/test/java/model/MoodModelTest.java
+++ src/test/java/model/MoodModelTest.java
@@ -1,19 +1,39 @@
 package model;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodModelTest {
-	private int moodScore;
-	
-	public MoodModelTest() {
-		//moodScore = 
+
+	@Test
+	public void initialTest() {
+		MoodModel mood = new MoodModel();
+		Assert.assertEquals(0, mood.getMoodScore());
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void integerTest() {
+		MoodModel mood = new MoodModel(Integer.MAX_VALUE);
+		Assert.assertEquals(Integer.MAX_VALUE, mood.getMoodScore());
 	}
 
+	@Test (expected = RuntimeException.class)
+	public void minusTest() {
+		MoodModel mood = new MoodModel(Integer.MIN_VALUE);
+		//Assert.assertEquals(Error, mood.getMoodScore());
+	}
+	
+	@Test
+	public void plusTest() {
+		MoodModel mood = new MoodModel(98);
+		mood.addition(5);
+		Assert.assertEquals(103, mood.getMoodScore());
+	}
+	
+	@Test
+	public void stringTest() {
+		MoodModel mood = new MoodModel("random");
+		Assert.assertTrue(mood.getMoodScoreAsString() instanceof String);
+	}
+	
 }
