52a09161d04bbf1afd38d4eae705e380d83be138
Norman A. Jiménez <jimen1@bfh.ch>
2021-01-19 19:14:52 +0100

    Added Diary commit method

--- src/main/java/persistence/DiaryRepository.java
+++ src/main/java/persistence/DiaryRepository.java
@@ -1,5 +1,24 @@
 package persistence;
 
+import model.common.Diary;
+
+import javax.persistence.EntityManager;
+
 //TO BE ALLOCATED PROPERLY
 public class DiaryRepository {
+
+    private EntityManager em;
+
+    public DiaryRepository() {
+        em = DBManager.getInstance().getEntityManager();
+    }
+
+    public void addDiary(Diary diary){
+        em.getTransaction().begin();
+        em.persist(diary);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+
 }
